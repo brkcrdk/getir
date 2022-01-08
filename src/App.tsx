4fcb2e49569af6bd308fb23e4d1cd29b190df3cc
Burak <burak@90pixel.com>
2022-01-09 00:06:34 +0300

    fetch products on page start.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useEffect } from "react";
 import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { actionTypes } from "types";
 import { Header, Container, Filters, Basket, Products } from "components";
 
+interface StateType {
+  productStore: {
+    filters: {
+      _limit: number;
+      _page: number;
+    };
+  };
+}
+
 function App() {
+  const { filters } = useSelector((s: StateType) => s.productStore);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: actionTypes.productStore.FETCH_PRODUCTS_REQUESTED,
+      payload: {
+        filters: {
+          ...filters,
+        },
+      },
+    });
+  }, []);
+
   return (
     <>
       <Header />
