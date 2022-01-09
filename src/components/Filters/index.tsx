9d47f8e32e55fefb3531a6c8a025ae59e362f342
Burak <burak@90pixel.com>
2022-01-10 00:08:38 +0300

    Add filter to sidebar.

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -2,10 +2,11 @@ import styled from "styled-components";
 import Sorting from "./Sorting";
 import Brands from "./Brands";
 import Tags from "./Tags";
+import { device } from "theme";
 
-const Filters = () => {
+const Filters = ({ sidebarFilter = false }) => {
   return (
-    <FiltersWrapper>
+    <FiltersWrapper sidebarFilter={sidebarFilter}>
       <Sorting />
       <Brands />
       <Tags />
@@ -15,11 +16,19 @@ const Filters = () => {
 
 export default Filters;
 
-const FiltersWrapper = styled.div`
+interface StyleProps {
+  sidebarFilter?: boolean;
+}
+
+const FiltersWrapper = styled.div<StyleProps>`
   width: 100%;
   height: 100%;
   max-width: 296px;
   display: grid;
   row-gap: 16px;
   transition: all 0.5s ease-in-out;
+  @media ${device.desktop} {
+    display: ${(p) => !p.sidebarFilter && "none"};
+    padding: 12px 6px;
+  }
 `;
