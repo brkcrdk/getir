6edf7af21947d23d285b1a9133af26d6ccd9a40b
Burak <burak@90pixel.com>
2022-01-10 18:03:09 +0300

    Get endpoint from env.

--- src/api/fetchProducts.ts
+++ src/api/fetchProducts.ts
@@ -4,7 +4,7 @@ import { FilterTypes, ResultType } from "types";
 const fetchProducts = async (filters: FilterTypes): Promise<ResultType> => {
   const customFilters = buildParams(filters);
 
-  const url = `https://getir-locals-api.herokuapp.com/products?${customFilters}`;
+  const url = `${process.env.REACT_APP_MAIN_URL}/products?${customFilters}`;
   const request = await fetch(url);
   const totalCount = Number(request.headers.get("X-Total-Count"));
   const totalPages = Math.ceil(totalCount / 16);
