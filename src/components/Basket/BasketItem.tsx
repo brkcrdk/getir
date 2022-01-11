32413c4f5bb47a512a332b0a673f199da9da6ee2
Burak <burak@90pixel.com>
2022-01-11 22:54:46 +0300

    Add basket item props.

--- src/components/Basket/BasketItem.tsx
+++ src/components/Basket/BasketItem.tsx
@@ -1,19 +1,39 @@
+import { FC } from "react";
 import styled from "styled-components";
 import { Icon } from "components";
-import theme from "theme";
-const BasketItem = () => {
+import theme, { lineClamp } from "theme";
+
+interface BasketItemProps {
+  price: number;
+  productName: string;
+  count: number;
+  incrementAmount: () => void;
+  decrementAmount: () => void;
+}
+
+const BasketItem: FC<BasketItemProps> = ({
+  price,
+  productName,
+  count,
+  incrementAmount,
+  decrementAmount,
+}) => {
   return (
     <BasketItemWrapper>
       <ItemInfo>
-        <span className="product-name">Example Product</span>
-        <span className="product-price">₺14,99</span>
+        <span title={productName} className="product-name">
+          {productName}
+        </span>
+        <span className="product-price">₺{price}</span>
       </ItemInfo>
       <AmountControllers>
-        <button>
+        <button title="Decrement Amount of Item" onClick={decrementAmount}>
           <Icon iconName="minus" size={14} color={theme.colors.main} />
         </button>
-        <span className="amount">1</span>
-        <button>
+        <span title="Amount of Items" className="amount">
+          {count}
+        </span>
+        <button title="Increment Amount of Item" onClick={incrementAmount}>
           <Icon iconName="plus" size={14} color={theme.colors.main} />
         </button>
       </AmountControllers>
@@ -27,12 +47,14 @@ const BasketItemWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  gap: 4px;
   border-bottom: 1px solid ${(p) => p.theme.colors.blackScale.black50};
   padding: 16px 8px;
   font-size: 14px;
   .product-name {
     color: ${(p) => p.theme.colors.grayScale.gray700};
     margin-bottom: 4px;
+    ${lineClamp(1)}
   }
   .product-price {
     color: ${(p) => p.theme.colors.main};
