8964822f75a01d3acd1ce24a2674c0b3e009fe12
Burak <burak@90pixel.com>
2022-01-12 00:39:36 +0300

    Bug fix try for sorting filter.

--- src/components/Filters/Sorting.tsx
+++ src/components/Filters/Sorting.tsx
@@ -4,31 +4,60 @@ import { useChangeFilter } from "hooks";
 
 import FilterContainer from "./FilterContainer";
 
+interface SortProps {
+  type: "_sort" | "_order";
+  value: "asc" | "desc" | "price" | "added";
+}
+interface SortTypes {
+  label: string;
+  id: string;
+  sortOptions: SortProps[];
+}
+
 const Sorting = () => {
   const [selectedSort, setSelected] = useState("price_asc");
   const { handleChangeFilter } = useChangeFilter();
 
-  const handleChange = (type: string) => {
-    handleChangeFilter("_sort", type);
-    return setSelected(type);
+  const handleChange = (selectedType: string, selectedSort: SortProps[]) => {
+    // selectedSort.forEach((sort) => handleChangeFilter(sort.type, sort.value));
+    return setSelected(selectedType);
   };
 
-  const sortTypes = [
+  const sortTypes: SortTypes[] = [
     {
       label: "Price low to high",
       id: "price_asc",
+      sortOptions: [
+        {
+          type: "_sort",
+          value: "price",
+        },
+        { type: "_order", value: "asc" },
+      ],
     },
     {
       label: "Price high to low",
       id: "price_desc",
+      sortOptions: [
+        { type: "_sort", value: "price" },
+        { type: "_order", value: "desc" },
+      ],
     },
     {
       label: "New to old",
-      id: "time_asc",
+      id: "added_asc",
+      sortOptions: [
+        { type: "_sort", value: "added" },
+        { type: "_order", value: "asc" },
+      ],
     },
     {
       label: "Old to new",
       id: "time_desc",
+      sortOptions: [
+        { type: "_sort", value: "added" },
+        { type: "_order", value: "desc" },
+      ],
     },
   ];
 
@@ -39,7 +68,7 @@ const Sorting = () => {
           key={type.id}
           label={type.label}
           id={type.id}
-          onChange={() => handleChange(type.id)}
+          onChange={() => handleChange(type.id, type.sortOptions)}
           checked={selectedSort === type.id}
           radioType="sortTypes"
         />
