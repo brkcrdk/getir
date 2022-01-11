e3595a41d8000367518d3d2eef3cdbb7f16b2158
Burak <burak@90pixel.com>
2022-01-11 18:22:51 +0300

    Add decrement and increment count reducers.

--- src/store/basketStore/reducer.ts
+++ src/store/basketStore/reducer.ts
@@ -1,4 +1,7 @@
-import { ProductType, BasketTypes, actionTypes } from "types";
+import { ProductType, BasketTypes, BasketType, actionTypes } from "types";
+
+const { ADD_TO_BASKET, REMOVE_FROM_BASKET, INCREMENT_COUNT, DECREMENT_COUNT } =
+  actionTypes.basketStore;
 
 const initialState = {
   basket: [],
@@ -9,10 +12,8 @@ interface ActionType {
   payload: ProductType;
 }
 
-const isSelected = (state: BasketTypes, action: ActionType) => {
-  return state.basket.find(
-    (basketItem) => basketItem.item.slug === action.payload.slug
-  );
+const isSelected = (basket: BasketType[], product: ProductType) => {
+  return basket.some((basketItem) => basketItem.item.slug === product.slug);
 };
 
 export const basketReducer = (
@@ -20,23 +21,59 @@ export const basketReducer = (
   action: ActionType
 ) => {
   switch (action.type) {
-    case actionTypes.basketStore.ADD_TO_BASKET:
-      if (!isSelected(state, action)) {
+    // Add To Basket
+    case ADD_TO_BASKET: {
+      if (!isSelected(state.basket, action.payload)) {
         const newBasketItem = { count: 1, item: action.payload };
         return {
           basket: state.basket.concat(newBasketItem),
         };
       }
       return state;
-    case actionTypes.basketStore.REMOVE_FROM_BASKET:
-      if (isSelected(state, action)) {
-        const updatedState = state.basket.filter(
+    }
+    // Remove from basket
+    case REMOVE_FROM_BASKET: {
+      if (isSelected(state.basket, action.payload)) {
+        const filteredState = state.basket.filter(
           (basketItem) => basketItem.item.slug !== action.payload.slug
         );
-        return { basket: updatedState };
+        return { basket: filteredState };
       }
       return state;
+    }
+    // Increment items count
+    case INCREMENT_COUNT: {
+      const updatedState = state.basket.map((basketItem) => {
+        if (basketItem.item.slug === action.payload.slug) {
+          return { ...basketItem, count: basketItem.count + 1 };
+        }
+        return basketItem;
+      });
+      return { basket: updatedState };
+    }
 
+    // Decrement items count
+    case DECREMENT_COUNT: {
+      const selectedItem = state.basket.find(
+        (basketItem) => basketItem.item.slug === action.payload.slug
+      );
+      // If selected items count is eq. to one remove that item
+      // Else decrease
+      if (selectedItem?.count === 1) {
+        const filteredState = state.basket.filter(
+          (basketItem) => basketItem.item.slug !== action.payload.slug
+        );
+        return { basket: filteredState };
+      } else {
+        const updatedState = state.basket.map((basketItem) => {
+          if (basketItem.item.slug === action.payload.slug) {
+            return { ...basketItem, count: basketItem.count - 1 };
+          }
+          return basketItem;
+        });
+        return { basket: updatedState };
+      }
+    }
     default:
       return state;
   }
