c9b699d503eb2b79e6add378ac90c99144c7df7b
Burak <burak@90pixel.com>
2022-01-12 11:00:18 +0300

    update useChangeFilter

--- src/hooks/useChangeFilter.ts
+++ src/hooks/useChangeFilter.ts
@@ -1,32 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
-import { actionTypes, StoreTypes } from "types";
-
-type filters =
-  | "_page"
-  | "_limit"
-  | "_sort"
-  | "_order"
-  | "manufacturer_like"
-  | "tags_like"
-  | "itemType";
+import { actionTypes, StoreTypes, FilterTypes } from "types";
 
 const useChangeFilter = () => {
   const dispatch = useDispatch();
   const { filters } = useSelector((s: StoreTypes) => s.productStore);
 
-  const handleChangeFilter = (
-    filterType: filters,
-    filterData: string | string[] | number
-  ) => {
+  const handleChangeFilter = (newFilters: FilterTypes) => {
     dispatch({
       type: actionTypes.productStore.UPDATE_PRODUCTS_REQUESTED,
       payload: {
         filters: {
           ...filters,
-          // Bu yapı yerine obje şeklinde gelmeli
-          // {...newFilters} gibi böylece order tarafındaki hata gider
-          // ayrıca daha mantıklı bir yapı oluşur.
-          [filterType]: filterData,
+          ...newFilters,
         },
       },
     });
